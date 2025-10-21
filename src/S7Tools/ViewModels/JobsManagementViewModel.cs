ec7f90eade3f7ea0ff70869bdfc1ca92768fcc92
Erik Fargas <efargas@users.noreply.github.com>
2025-10-21 17:23:52 +0200

    Revert "feat: Complete Job Management Enhancement - Job Info Display + Panel Layout Refactor"

--- src/S7Tools/ViewModels/JobsManagementViewModel.cs
+++ src/S7Tools/ViewModels/JobsManagementViewModel.cs
@@ -12,7 +12,6 @@ using S7Tools.Core.Services.Interfaces;
 using S7Tools.Core.Validation;
 using S7Tools.Services.Interfaces;
 using S7Tools.ViewModels.Base;
-using S7Tools.ViewModels.Jobs;
 
 namespace S7Tools.ViewModels;
 
@@ -67,11 +66,6 @@ public class JobsMainContentViewModel : ViewModelBase, IDisposable
             .Subscribe(_ => this.RaisePropertyChanged(nameof(UserJobs)))
             .DisposeWith(_disposables);
     }
-
-    /// <summary>
-    /// Gets the JobInfoDisplayViewModel for the job details panel.
-    /// </summary>
-    public ViewModels.Jobs.JobInfoDisplayViewModel? JobInfoDisplayViewModel { get; internal set; }
 
     // Expose parent properties for data binding
     public ObservableCollection<JobProfile> Profiles => _parent.Profiles;
@@ -631,28 +625,8 @@ public class JobsManagementViewModel : ProfileManagementViewModelBase<JobProfile
             // This prevents circular references by not returning 'this'
             _logger.LogDebug("Creating main jobs content ViewModel");
 
-            // Create the wrapper ViewModel
-            var contentViewModel = new JobsMainContentViewModel(this);
-
-            // Create the JobInfoDisplayViewModel using the factory if available
-            if (_viewModelFactory != null)
-            {
-                try
-                {
-                    contentViewModel.JobInfoDisplayViewModel = _viewModelFactory.Create<JobInfoDisplayViewModel>();
-                    _logger.LogDebug("Successfully created JobInfoDisplayViewModel via factory");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogWarning(ex, "Failed to create JobInfoDisplayViewModel via factory");
-                }
-            }
-            else
-            {
-                _logger.LogDebug("ViewModelFactory not available for JobInfoDisplayViewModel creation");
-            }
-
-            return contentViewModel;
+            // Return wrapper ViewModel that will be resolved to JobsMainContentView by ViewLocator
+            return new JobsMainContentViewModel(this);
         }
         catch (Exception ex)
         {
